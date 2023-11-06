a7519d09f253a6d4dc7f06cd617ef095111b932c
Emil <emilkaramihov@gmail.com>
2023-11-06 12:35:57 +0200

    Added view model disposable

--- lib/app/utils/state_management/vm_builder.dart
+++ lib/app/utils/state_management/vm_builder.dart
@@ -38,4 +38,10 @@ class _VMBuilderState<T extends BaseVM> extends State<VMBuilder<T>> {
     builder: widget.builder,
     child: widget.child,
   );
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
 }
